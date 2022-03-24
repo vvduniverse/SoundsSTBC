6f45d2cbeaddb0791da8384414bf934459f70a21
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-24 17:55:15 +0200

    Initial Commit

---
+++ SoundsSTBC/SoundsSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundsSTBCApp.swift
+//  SoundsSTBC
+//
+//  Created by Vahtee Boo on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SoundsSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
